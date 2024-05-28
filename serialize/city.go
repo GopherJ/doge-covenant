435d3d7c13915bb029a76e2680cd84e5227ff189
GopherJ <alex_cj96@foxmail.com>
2024-05-28 11:22:32 +0800

    feat: add toJsonCityVk
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- serialize/city.go
+++ serialize/city.go
@@ -6,12 +6,20 @@ import (
 )
 
 type CityGroth16ProofData struct {
-	PiA   string `json:"pi_a"`
-	PiBA0 string `json:"pi_b_a0"`
-	PiBA1 string `json:"pi_b_a1"`
-	PiC   string `json:"pi_c"`
-  PublicInput0 string `json:"public_input_0"`
-  PublicInput1 string `json:"public_input_1"`
+	PiA          string `json:"pi_a"`
+	PiBA0        string `json:"pi_b_a0"`
+	PiBA1        string `json:"pi_b_a1"`
+	PiC          string `json:"pi_c"`
+	PublicInput0 string `json:"public_input_0"`
+	PublicInput1 string `json:"public_input_1"`
+}
+
+type CityGroth16VerifierData struct {
+	AlphaG1 string   `json:"alpha_g1"`
+	BetaG2  string   `json:"beta_g2"`
+	GammaG2 string   `json:"gamma_g2"`
+	DeltaG2 string   `json:"delta_g2"`
+	G1K     []string `json:"k"`
 }
 
 func ToJsonCityProof(p *groth16_bls12381.Proof, witness []fr.Element) (*CityGroth16ProofData, error) {
@@ -39,7 +47,48 @@ func ToJsonCityProof(p *groth16_bls12381.Proof, witness []fr.Element) (*CityGrot
 		piBSerialized[:96],
 		piBSerialized[96:],
 		piCSerialized,
-    ReverseHexString(arkHex2Proof.Witness[0]),
-    ReverseHexString(arkHex2Proof.Witness[1]),
+		ReverseHexString(arkHex2Proof.Witness[0]),
+		ReverseHexString(arkHex2Proof.Witness[1]),
+	}, nil
+}
+
+func ToJsonCityVK(vk *groth16_bls12381.VerifyingKey) (*CityGroth16VerifierData, error) {
+	arkVk := ToJsonArkVK(vk)
+
+	arkHex2VK := arkVk.ToArkHex2VK()
+
+	AlphaG1, err := SerializeG1(&arkHex2VK.AlphaG1)
+	if err != nil {
+		return nil, err
+	}
+
+	var G1K []string
+	for _, g1k := range arkHex2VK.G1K {
+		g1k, err := SerializeG1(&g1k)
+		if err != nil {
+			return nil, err
+		}
+		G1K = append(G1K, g1k)
+	}
+
+	BetaG2, err := SerializeG2(&arkHex2VK.BetaG2)
+	if err != nil {
+		return nil, err
+	}
+	GammaG2, err := SerializeG2(&arkHex2VK.GammaG2)
+	if err != nil {
+		return nil, err
+	}
+	DeltaG2, err := SerializeG2(&arkHex2VK.DeltaG2)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CityGroth16VerifierData{
+		AlphaG1,
+		BetaG2,
+		GammaG2,
+		DeltaG2,
+		G1K,
 	}, nil
 }
